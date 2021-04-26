cf6e6bb35f3eb20b8b76d97185c315e57cc9ad62
sbhaga5 <67068151+sbhaga5@users.noreply.github.com>
2021-04-26 00:36:16 -0400

    Update NM ERB Pension Wealth Model.R

--- NM ERB Pension Wealth Model.R
+++ NM ERB Pension Wealth Model.R
@@ -17,6 +17,8 @@ ARR <- as.double(model_inputs[which(model_inputs[,1] == 'ARR/DR'),2])
 Interest <- as.double(model_inputs[which(model_inputs[,1] == 'Credited Interest'),2])
 COLA <- as.double(model_inputs[which(model_inputs[,1] == 'COLA'),2])
 COLACompound <- as.double(model_inputs[which(model_inputs[,1] == 'COLA Compounds (1=Yes)'),2])
+ScaleMultiple <- as.double(model_inputs[which(model_inputs[,1] == 'Mortality Rate Scale Multiple'),2])
+SetBackYear <- as.double(model_inputs[which(model_inputs[,1] == 'Mortality Rate Set Back (year)'),2])
 #
 #
 #Linear Interpolation Function. Use for later
@@ -46,16 +48,19 @@ LinearInterpolation <- function(Data){
   
   return(Data)
 }
-#
+
+InitializeDataFieldName <- function (Data, ColumnName) {
+  NewData <- Data
+  colnames(NewData) <- ColumnName
+  return(NewData)
+}
 #
 #Salary increases and other
-MeritIncreases <- LinearInterpolation(MeritIncreases)
-Salary <- MeritIncreases
-colnames(Salary) <- 'Salary Growth'
-
+#MeritIncreases <- LinearInterpolation(MeritIncreases)
+Salary <- InitializeDataFieldName(MeritIncreases, 'Salary Growth')
 Salary[1,1] <- StartingSalary
-FinalAvgSalary <- MeritIncreases
-colnames(FinalAvgSalary) <- 'Final Average Salary Growth'
+FinalAvgSalary <- InitializeDataFieldName(MeritIncreases,'Final Average Salary Growth')
+
 for(i in 1:nrow(MeritIncreases)){
   if(i > 1){
     Salary[i,1] <- Salary[i-1,1]*(1 + (MeritIncreases[i-1,1] + payroll_growth)) 
@@ -69,15 +74,10 @@ for(i in 1:nrow(MeritIncreases)){
 #
 #
 #Employee Contribution
-Employee_Contrib <- Salary*EE_Contrib
-colnames(Employee_Contrib) <- 'Employee Contribution'
-Employer_Contrib <- Salary*ER_Contrib
-colnames(Employer_Contrib) <- 'Employer Contribution'
-
-Employee_Contrib_CI <- Salary*EE_Contrib
-colnames(Employee_Contrib) <- 'Employee Contribution (with Compounded Interest)'
-PV_Employee_Contrib_CI <- Employee_Contrib_CI
-colnames(PV_Employee_Contrib_CI) <- 'PV of Employee Contribution (with Compounded Interest)'
+Employee_Contrib <- EE_Contrib*InitializeDataFieldName(Salary,'Employee Contribution')
+Employee_Contrib_CI <- EE_Contrib*InitializeDataFieldName(Salary,'Employee Contribution (with Compounded Interest)')
+PV_Employee_Contrib_CI <- InitializeDataFieldName(Employee_Contrib_CI,'PV of Employee Contribution (with Compounded Interest)')
+Employer_Contrib <- ER_Contrib*InitializeDataFieldName(Salary,'Employer Contribution')
 
 for(i in 1:nrow(PV_Employee_Contrib_CI)){
   if((YOS[i,1] > 0) && (i > 1)){
@@ -113,10 +113,10 @@ RP2014DisFemale <- SurvivalRates[,13]
 
 Pub2010NonDisMaleTeacher <- LinearInterpolation(Pub2010NonDisMaleTeacher)
 Pub2010DisMaleTeacher <- LinearInterpolation(Pub2010DisMaleTeacher)
-Pub2010MaleTeacher <- LinearInterpolation(Pub2010MaleTeacher)
+#Pub2010MaleTeacher <- LinearInterpolation(Pub2010MaleTeacher)
 Pub2010NonDisFemaleTeacher <- LinearInterpolation(Pub2010NonDisFemaleTeacher)
 Pub2010DisFemaleTeacher <- LinearInterpolation(Pub2010DisFemaleTeacher)
-Pub2010FemaleTeacher <- LinearInterpolation(Pub2010FemaleTeacher)
+#Pub2010FemaleTeacher <- LinearInterpolation(Pub2010FemaleTeacher)
 
 YearCol <- (2129 - 2009 + 1)
 AgeRow <- (120 - 25 + 1)
@@ -127,37 +127,20 @@ for(i in 1:96){
   SurvivalAge <- i + 24
   for(j in 2:121){
     SurvivalYear <- j + 2008
+    #Column Check is for all of the special cases such as RP 2014 and Pub 2010
     ColumnCheck <- 0
     
     if((SurvivalAge <= 75) && (SurvivalYear == 2010)){
       SurvivalMaleMatrix[i,j] <- as.double(Pub2010MaleTeacher[which(SurvivalRates[,1] == SurvivalAge),1])
       SurvivalFemaleMatrix[i,j] <- as.double(Pub2010FemaleTeacher[which(SurvivalRates[,1] == SurvivalAge),1])
       ColumnCheck <- 1
-    } else if ((SurvivalAge > 80) && (SurvivalYear == 2010)){
-      SurvivalMaleMatrix[i,j] <- as.double(RP2014HealthyMale[which(SurvivalRates[,1] == SurvivalAge),1])
-      SurvivalFemaleMatrix[i,j] <- as.double(RP2014HealthyFemale[which(SurvivalRates[,1] == SurvivalAge),1])
-      ColumnCheck <- 1
-    } else if (SurvivalYear == 2014){
-      if(SurvivalAge < 50){
-        SurvivalMaleMatrix[i,j] <- as.double(RP2014Male[which(SurvivalRates[,1] == SurvivalAge),1])
-        SurvivalFemaleMatrix[i,j] <- as.double(RP2014Female[which(SurvivalRates[,1] == SurvivalAge),1])
-      } else {
-        SurvivalMaleMatrix[i,j] <- as.double(RP2014HealthyMale[which(SurvivalRates[,1] == SurvivalAge),1])
-        SurvivalFemaleMatrix[i,j] <- as.double(RP2014HealthyFemale[which(SurvivalRates[,1] == SurvivalAge),1])
-      }
-    } else if((SurvivalAge > 75) && (SurvivalYear == 2010)){
-      Value1 <- as.double(Pub2010MaleTeacher[which(SurvivalRates[,1] == 75),1])
-      Value2 <- as.double(RP2014HealthyMale[which(SurvivalRates[,1] == 81),1])
-      Slope <- (Value2 - Value1) / (81 - 75)
-      SurvivalMaleMatrix[i,j] <- (Slope*(SurvivalAge - 75)) + Value1
-      
-      Value1 <- as.double(Pub2010FemaleTeacher[which(SurvivalRates[,1] == 75),1])
-      Value2 <- as.double(RP2014HealthyFemale[which(SurvivalRates[,1] == 81),1])
-      Slope <- (Value2 - Value1) / (81 - 75)
-      SurvivalFemaleMatrix[i,j] <- (Slope*(SurvivalAge - 75)) + Value1
+    } else if ((SurvivalAge > 75) && ((SurvivalYear >= 2010) && (SurvivalYear <= 2014))){
+      SurvivalMaleMatrix[i,j] <- as.double(RP2014HealthyMale[which(SurvivalRates[,1] == SurvivalAge),1])*ScaleMultiple
+      SurvivalFemaleMatrix[i,j] <- as.double(RP2014HealthyFemale[which(SurvivalRates[,1] == SurvivalAge),1])*ScaleMultiple
       ColumnCheck <- 1
     }
     
+    #If ColumnCheck is 0, then do the normal calc
     if(ColumnCheck == 0){
       if(SurvivalYear <= 2033){
         YearRef <- SurvivalYear
@@ -211,8 +194,8 @@ for(i in 1:nrow(RPRatesMale)){
     AdjustedMale[i,1] <- SurvivalMaleMatrix[i,11]
     AdjustedFemale[i,1] <- SurvivalFemaleMatrix[i,11]
   } else {
-    AdjustedMale[i,1] <- SurvivalMaleMatrix[i,11+i-1]
-    AdjustedFemale[i,1] <- SurvivalFemaleMatrix[i,11+i-1]
+    AdjustedMale[i,1] <- SurvivalMaleMatrix[i,11+i]
+    AdjustedFemale[i,1] <- SurvivalFemaleMatrix[i,11+i]
     AdjustedAvg[i,1] <- (AdjustedMale[i,1] + AdjustedFemale[i,1]) / 2
   }
 }
@@ -220,16 +203,11 @@ for(i in 1:nrow(RPRatesMale)){
 #
 #Survival Probability and Annuity Factor
 SurvivalRates <- AdjustedAvg
-
-ProbNextYear <- SurvivalRates
-colnames(ProbNextYear) <- 'Probability of Survival to Next Year'
+InitializeDataFieldName
+ProbNextYear <- InitializeDataFieldName(SurvivalRates,'Probability of Survival to Next Year')
 ProbNextYear[1,1] <- 1
-
-DiscProbNextYear <- ProbNextYear
-colnames(DiscProbNextYear) <- 'Discount Probability of Survival to Next Year'
-
-LifeExpectancy <- ProbNextYear
-colnames(LifeExpectancy) <- 'Life Expectancy'
+DiscProbNextYear <- InitializeDataFieldName(ProbNextYear,'Discount Probability of Survival to Next Year')
+LifeExpectancy <- InitializeDataFieldName(ProbNextYear,'Life Expectancy')
 
 for(i in 1:nrow(ProbNextYear)){
   if(i == 1){
@@ -248,23 +226,23 @@ AnnuityFactor <- DiscProbNextYear
 colnames(AnnuityFactor) <- 'Annuity Factor'
 AnnuityFactor[1:20,1] <- 1
 
-for(j in 45:114){
+for(j in 45:120){
   sum <- 0
   RowIndex <- j - 25 + 1
-  for(i in RowIndex:nrow(DiscProbNextYear)-5){
+  TempValue <- 0
+  for(i in RowIndex:nrow(DiscProbNextYear)){
     if((as.double(Age[i,1]) >= j) && (COLACompound == 1)){
       TempValue <- as.double(DiscProbNextYear[i,1])*((1+COLA)^(as.double(Age[i,1]) - j))
       sum <- sum + TempValue
     } else {
-      TempValue <- as.double(DiscProbNextYear[i,1])*(1 +(COLA)*(as.double(Age[i,1]) - j))
+      TempValue <- as.double(DiscProbNextYear[i,1])*(1+(COLA*(as.double(Age[i,1]) - j)))
       sum <- sum + TempValue
     }
-    
     
     if(as.double(Age[i,1]) == j){
       FactorDiv <- TempValue
     }
   }
-
+  
   AnnuityFactor[RowIndex,1] <- sum / FactorDiv
 }
